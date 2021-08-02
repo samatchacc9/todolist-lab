b4b8eebae24f2f36c1c2bc50c236a73591b2c6e0
samatchacc9 <samatcha.matcha@gmail.com>
2021-08-03 05:34:50 +0700

    v1

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@
 // import Header from "./components/Header";
 // import Footer from "./components/Footer";
 // import Menu from "./components/Menu";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div>
-      <button>Click to hind text</button>
-      <h1>Text</h1>
-    </div>
-  );
+  const [show, setShow] = useState(true);
+
+  const clickHide = e => {
+    setShow;
+  };
+  return <button onClick={clickHide}>Click to hide me</button>;
 }
 
 export default App;
--- src/Example.js
+++ src/Example.js
@@ -4,6 +4,8 @@
 // import Header from "./components/Header";
 // import Footer from "./components/Footer";
 // import Menu from "./components/Menu";
+import "./App.css";
+import { useState } from "react"; //import function มาใช้จาก Library
 
 function App() {
   // const handleChangCheckbox = event => {
@@ -12,12 +14,60 @@ function App() {
   //   console.log(event.target.checked ? "Tick" : "Unticked"); // ค่า Tick
   // };
 
-  const handleOnClick = event => {
-    event.preventDefault();
-    const press = window.confirm("You want to google");
-    if (press === true) {
-      window.location.assign("https://google.com");
-    }
+  //6.4
+  // const handleOnClick = event => {
+  //   event.preventDefault();
+  //   const press = window.confirm("You want to google");
+  //   if (press === true) {
+  //     window.location.assign("https://google.com");
+  //   }
+  // };
+
+  //=====State=====//
+  // console.log("App is called");
+  // let text = "Hello React";
+
+  // const result = useState("Hello React!!!"); // เก็บข้อมูล satateค่าเริ่มต้น
+  // console.log(result);
+  //เขียน แบบยาวไม่นิยม
+  /*
+  const state = result[0]; //Hello React state ปัจจุบัน
+  const update = result[1]; //ตัว update
+  */
+
+  //state advance style destructuring เขียนแบบสั้น  not good
+  /*
+  const [state, update] = useState("Hello Reacct!!!");
+  const [state1, update] = useState("Hello Reacct!!!");
+  const [state2, update] = useState("Hello Reacct!!!");
+
+  //Best Practice
+  const [message, setMessage] = useState("Hello React");
+  const [title, setTitle] = useState("My Title");
+
+  const handleOnClick = () => {
+    // update("You clicked a button"); // เมื่อ click ให้มาฟังก์ชั่นที่ set
+
+    setMessage("You clicked a button");
+
+    // Wrong
+    // console.log("clicked");
+    // text = "You clicked a button";
+    // console.log(text);
+    
+  };
+*/
+
+  //State 2
+  const [count, setCount] = useState(0); // เริ่มต้นเท่ากับ 0
+
+  const handleClickAdd = () => {
+    // setCount(count + 1); // มีโอกาสผิดพลาด
+    setCount(currentState => currentState + 1); // อ้างอิงจากค่าปัจจุบัน  currentState เป็น paramiter function  setCount
+  };
+  const handleClickSubtract = () => {
+    // setCount(count - 1);
+    setCount(currentCount => currentCount - 1);
   };
 
   return (
@@ -130,11 +180,44 @@ function App() {
     // </div>
 
     //6.4
-    <div>
-      <a href="https://google.com" onClick={handleOnClick}>
-        Goto Google
-      </a>
-    </div>
+    // <div>
+    //   <a href="https://google.com" onClick={handleOnClick}>
+    //     Goto Google
+    //   </a>
+    // </div>
+
+    //State
+
+    // <div>
+    //   <h1>{message}</h1>
+    //   <button onClick={handleOnClick}>Click to change Text</button>
+    //   <button onClick={() => setMessage("You Clicked Annonymous")}>
+    //     Click to change Text
+    //   </button>
+    // </div>
+
+    //State 2
+    // <div className="container">
+    //   <button onClick={handleClickAdd}>+</button>
+    //   <span>{count}</span>
+    //   <button onClick={handleClickSubtract}>-</button>
+    // </div>
+    // );
+
+    //state 3 Multiple
+    //  const [username, setUsername] = useState('');
+    //  const [password, setPassword] = useState('');
+    //  const [email, setEmail] = useState('');
+    //  const [birthDate, setBirthDate, = useState('');
+
+    // Group state ทำการยุบ
+    //  const [user, setUser] = useState({
+    //    username: '',
+    //    password: '',
+    //    email: '',
+    //    birthDate: '',
+
+    <div></div>
   );
 }
 
